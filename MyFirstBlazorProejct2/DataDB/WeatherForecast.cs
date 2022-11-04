5e142395ade0e50418d71c787940926de138a03e
DESKTOP-UL9R65A\Alexander <Alexanderrasmussen10@hotmail.com>
2022-11-04 14:25:31 +0100

    init

---
+++ MyFirstBlazorProejct2/DataDB/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyFirstBlazorProejct2.DataDB
+{
+    public partial class WeatherForecast
+    {
+        public DateTime? Date { get; set; }
+        public int? TemperatureC { get; set; }
+        public string? Summary { get; set; }
+        public int? TemperatureF { get; set; }
+    }
+}
